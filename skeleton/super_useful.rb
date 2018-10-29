331d8f33686c26b09240a06fa660fab91801bee2
kat-onyx <sarah.kathryn.peters@gmail.com>
2018-10-29 13:58:59 -0700

    Add retry to #feed_me_a_fruit

--- skeleton/super_useful.rb
+++ skeleton/super_useful.rb
@@ -20,10 +20,13 @@ end
 
 def feed_me_a_fruit
   puts "Hello, I am a friendly monster. :)"
-
-  puts "Feed me a fruit! (Enter the name of a fruit:)"
-  maybe_fruit = gets.chomp
-  reaction(maybe_fruit)
+  begin
+    puts "Feed me a fruit! (Enter the name of a fruit:)"
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit)
+  rescue StandardError
+    retry if maybe_fruit == "coffee"
+  end
 end
 
 # PHASE 4
